cff9fd4f4fbb4e55a81dea12b03ef1bc50d6058c
deanfoulds <deanfoulds@gmail.com>
2018-09-12 16:32:50 +0100

    user redirected to home, soon to be profile page of app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth'
 
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(private afAth: AngularFireAuth, public toast: ToastController,
+    public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  ionViewDidLoad() {
+    this.afAth.authState.subscribe(data => {
+      if (data.email && data.uid) {
+        this.toast.create({
+          message: 'Welcome ${this.user.email} to your profile',
+          duration: 4000
+        }).present();
+      }
+      else {
+        this.toast.create({
+          message: 'You are not authenticated',
+          duration: 4000
+        }).present();
+      }
+    }
+    );
+  }
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { User } from '../../models/user';
+import { AngularFireAuth } from 'angularfire2/auth'
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the LoginPage page.
@@ -18,11 +20,24 @@ export class LoginPage {
 
   user = {} as User;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private afAuth: AngularFireAuth,
+    public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  async login(user: User) {
+    try {
+      const result = this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password);
+      console.log(result);
+      if (result) {
+        this.navCtrl.setRoot('HomePage');
+      }
+    }
+    catch (e) {
+      console.error(e);
+    }
   }
 
+  register() {
+    this.navCtrl.push('RegisterPage');
+  }
 }
